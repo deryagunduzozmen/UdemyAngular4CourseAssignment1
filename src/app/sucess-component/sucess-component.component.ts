815df0115c0c5b20bb72aca23e29cb21fcf51cfd
deryagunduzozmen <derya.gunduzozmen@outlook.com>
2017-05-26 17:39:36 +0300

    assignment1 developed

---
+++ src/app/sucess-component/sucess-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sucess-component',
+  templateUrl: './sucess-component.component.html',
+  styleUrls: ['./sucess-component.component.css']
+})
+export class SucessComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
